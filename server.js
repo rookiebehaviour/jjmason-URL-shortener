ad2b32462175c0c378bf9a5a28c3a82682ba05ce
Jacqueline <75797321+rookiebehaviour@users.noreply.github.com>
2021-01-30 16:44:58 -0500

    Fixed bug

--- server.js
+++ server.js
@@ -3,14 +3,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const dns = require('dns');
 const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(bodyParser.urlencoded({ entended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -29,17 +28,18 @@ app.listen(port, function() {
 
 //Database Connection
 mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true});
-let urlSchema = new mongoose.Schema({ original : {type: String, required: true}, short : Number });
-let Url = mongoose.model('Url', urlSchema);
+
+const urlSchema = new mongoose.Schema({ original : {type: String, required: true}, short : Number });
+const Url = mongoose.model('Url', urlSchema);
 let responseObject = {};
 
 app.post('/api/shorturl/new', bodyParser.urlencoded({ extended: false }), (req, res) => {
   let inputUrl = request.body['url']
-  let urlRegex = new RegExp(/^(?![0-9]+$)(?!.*-$)(?!-)[a-zA-Z0-9-]{1,63}$/g);
-    if(!inputUrl.match(urlRegex)) {
-      response.json({error: 'Invalid URL'})
-      return
-    }
+  let urlRegex = new RegExp(/^(?![0-9]+$)(?!.*-$)(?!-)[a-zA-Z0-9-]{1,63}$/gi);
+  if(!inputUrl.match(urlRegex)) {
+    response.json({error: 'Invalid URL'})
+    return
+  }
   responseObject['original_url'] = inputUrl;
 
   let inputShort = 1;
@@ -66,13 +66,13 @@ app.post('/api/shorturl/new', bodyParser.urlencoded({ extended: false }), (req, 
 });
 
 app.get('/api/shorturl/:input', (req, res) => {
-  let input = request.params.inputUrl
+  let input = request.params.inputUrl;
 
   Url.findOne({short: input}, (error, result) => {
     if(!error && result != undefined) {
       response.redirect(result.original)
     } else {
-      response.json('URL not Found')
+      response.json('URL not Found');
     }
   })
 });
